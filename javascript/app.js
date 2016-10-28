a0eca54b99f7fd9c993568e96c1e29d89f2e6fe3
Allan Guggenheim <allan.guggenheim@gmail.com>
2016-10-27 17:38:13 -0700

    Game finally works

---
+++ javascript/app.js
@@ -0,0 +1,60 @@
+
+var timerLive = true;
+
+var timerVar = setInterval(countTimer, 1000);
+var totalSeconds = 101;
+function countTimer() {
+--totalSeconds;
+
+
+if (timerLive){
+
+	if (totalSeconds == 0) {
+		window.location.href = "outOfTime.html";
+	}
+
+	document.getElementById("timer").innerHTML = totalSeconds;
+	}
+
+}
+
+function contains(a, obj) {
+    var i = a.length;
+    while (i--) {
+       if (a[i] === obj) {
+           return true;
+       }
+    }
+    return false;
+}
+
+var questionsCount=4;
+var questionsButtonsClicked=0;
+
+
+function handleClick()
+  {         
+var answers = ["q1-d", "q2-d", "q3-d", "q4-d"]
+var amountCorrect = 0;          
+for(var i = 1; i <= 45; i++) {
+  var radios = document.getElementsByName('q'+i);
+  for(var j = 0; j < radios.length; j++) {
+    var radio = radios[j];
+    if (radio.checked) {
+    	++questionsButtonsClicked;
+    }
+    // console.log(radio.value);
+    if(contains(answers, radio.value) && radio.checked) {
+      amountCorrect++;
+    }
+  }
+ }  
+ 	timerLive = false;
+    console.log("Correct Responses: " + amountCorrect);
+	document.getElementById("quiz").style.display="none";
+	document.getElementById("RESULTS").innerHTML = "All Done!<br>Correct Answers: " 
+		+ amountCorrect + "<br>Incorrect Answers: "
+		+ (questionsCount - amountCorrect) + "<br>Unanswered: "
+		+ (questionsCount - questionsButtonsClicked);
+  }
+
